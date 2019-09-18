9ede8c3a995e455339b7c3a524023c8e3fdcf17c
roguewei <1104837637@qq.com>
2019-09-18 15:01:55 +0800

    firest commit

---
+++ shiro-core/src/main/java/com/winston/service/IPermissionService.java
@@ -0,0 +1,15 @@
+package com.winston.service;
+
+import com.winston.entity.Permission;
+
+import java.util.List;
+
+public interface IPermissionService {
+
+    List<Permission> queryAll();
+
+    List<Permission> queryByUserName(String username);
+
+    void addAllUrl(Permission permission);
+
+}
